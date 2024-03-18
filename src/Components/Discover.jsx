2c4f49e45b66e7442703264d6be64fa8e3953313
Shahid khan <professionalshahid0@gmail.com>
2024-03-18 15:38:40 +0530

    .

--- src/Components/Discover.jsx
+++ src/Components/Discover.jsx
@@ -1,6 +1,6 @@
 function ParallaxSection() {
   return (
-    <div className="w-full h-[55vh] bg-Discover bg-cover  bg-fixed  flex items-center justify-center mt-10">
+    <div className="w-full h-[55vh] bg-Discover bg-cover bg-center  bg-fixed  flex items-center justify-center mt-10">
       <div className="z-9 flex items-center justify-center flex-col text-wrap ">
         <h2 className="font-Courgette text text-yellow-300 text-8xl sm:text-5xl">
           Discover
